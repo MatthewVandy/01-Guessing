b7949c283a868fa47304b2a1b559c2c35b381d64
MatthewVandy <59939756+MatthewVandy@users.noreply.github.com>
2020-01-25 14:10:37 -0500

    Update main.py
    Added count to main py

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
 
 xfactor = random.randint(1,10)
 yfactor = -1
-while xfactor != yfactor:
+count = 0
 
+while xfactor != yfactor:
+    count= count+1
     yfactor = input("what is the number?")
     yfactor = int(yfactor)
     if xfactor == yfactor:
@@ -14,4 +16,4 @@ while xfactor != yfactor:
         print("try again choose bigger number")
     elif xfactor < yfactor:
         print("try again choose smaller number")
-
+print(str(count)+" amount of times tried")
